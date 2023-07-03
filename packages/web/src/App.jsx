228dfacbf6ae35245e6e9c74f7b4ca2419bfd613
jamespyle1990 <53844416+JamesPyle1990@users.noreply.github.com>
2023-07-03 02:51:36 +0000

    Added a line height slider, Added chord information with the proper spacing

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -2,12 +2,43 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
 import FormatPage from './FormatPage';
 import LyricsInput from './LyricsInput';
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
 
+
+let theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1024, // our custom breakpoint
+      xl: 1920,
+    },
+  },
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#513466',
+    },
+    secondary: {
+      main: '#0f0f12',
+    },
+    text: {
+      secondary: '#ffffff',
+    },
+  },
+  props: {
+    MuiAppBar: {
+      color: 'secondary',
+    },
+  },
+});
 
 function App() {
  
   return (
     <div>
+      <ThemeProvider theme={theme}>
     <div>
      <BrowserRouter>
         <Routes>
@@ -15,6 +46,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </div>
+    </ThemeProvider>
   </div>
   );
 }
