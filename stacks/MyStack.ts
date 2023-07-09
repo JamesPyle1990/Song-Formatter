f97da0e7e5aaf3968dec65750d548723e948a1dd
James Pyle <JPyle@manifestcorp.com>
2023-07-09 14:42:57 -0400

    fixed sst config for us-east-2 and fixed MyStack to return api for the WebStack

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,28 +1,16 @@
-import { StackContext, Api, EventBus } from "sst/constructs";
+import { StackContext, Api } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
-  const bus = new EventBus(stack, "bus", {
-    defaults: {
-      retries: 10,
-    },
-  });
-
   const api = new Api(stack, "api", {
-    defaults: {
-      function: {
-        bind: [bus],
-      },
-    },
     routes: {
       "GET /": "packages/functions/src/lambda.handler",
     },
   });
-
-  bus.subscribe("todo.created", {
-    handler: "packages/functions/src/events/todo-created.handler",
-  });
-
   stack.addOutputs({
     ApiEndpoint: api.url,
   });
+
+  return {
+    api
+  }
 }
